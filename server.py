2e85f47a46064a8e244ce27b5f8f54f0a23883d4
adsyf <adsyf@hotmail.com>
2025-07-21 10:49:33 +0100

    added separate config

--- server.py
+++ server.py
@@ -2,12 +2,14 @@ import asyncio
 from urllib import response
 import websockets
 import logging
+from config import *
 
 logger = logging.getLogger(__name__)
 class WebSocketServer:
     def __init__(self):
         self.clients = set()
         self.server = None
+        self.cfg = get_env_config()
 
     async def _handle(self,websocket):
         logger.info("client connected")
@@ -19,7 +21,7 @@ class WebSocketServer:
 
     async def start_server(self):
         logger.info("starting server")
-        server = await websockets.serve(self._handle, "localhost", 8765)
+        server = await websockets.serve(self._handle, self.cfg.websocket.host, self.cfg.websocket.port)
         logger.info("server started")
         await asyncio.sleep(0.1)
         logger.info("finished sleep")
--- test_websocket.py
+++ test_websocket.py
@@ -5,6 +5,7 @@ import websockets
 import logging
 from server import WebSocketServer, logger
 import sys
+from config import *
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,6 +14,9 @@ logging.basicConfig(level=logging.INFO,
                     )
 
 logger.setLevel(logging.INFO)
+os.environ["ENV"] = "dev"
+cfg = get_env_config()
+
 
 @pytest.fixture
 async def websocket_server():
@@ -24,6 +28,7 @@ async def websocket_server():
 
 @pytest.mark.asyncio
 async def test_websocket_connection(websocket_server):
-    uri = "ws://localhost:8765"
+    #uri = "ws://localhost:8765"
+    uri = cfg.websocket.get_url()
     async with websockets.connect(uri) as websocket:
         assert websocket.state.name == 'OPEN'
